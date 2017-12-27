8691a4fbddfe918f2d92e6f000cfc5c93a7db9b4
Jason Caldwell <jdcald13@gmail.com>
2017-12-27 16:50:16 -0700

    Divide logic into separate functions

--- js/blackjack.js
+++ js/blackjack.js
@@ -24,14 +24,11 @@ function placeBet(){
   return bet;
 }
 
-// Setup game
-var initialDeal = function() {
+// Create new shuffled deck
+function create_shuffled_deck(){
   // Create ranks and suits
   var rank = ["2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"];
   var suit = ["c", "d", "h", "s"];
-
-  $('#dealer_name').text(dealer.name + "'s Hand Value: ");
-  $('#player_name').text(player.name + "'s Hand Value: ");
 
   // Create deck
   var deck = function(){
@@ -60,89 +57,72 @@ var initialDeal = function() {
     }
     return array;
   };
+  return shuffle(new_deck);
+}
 
-  shuffle(new_deck);
+// Deal initial cards (2 for the player and one for the dealer)
+function dealHand(){
+  return cards.shift();
+}
 
-  // Deal initial cards (2 for the player and one for the dealer)
-  function dealHand(){
-    return cards.shift();
-  }
+// Show card image
+function displayCard(card) {
+  var image_location = '<img src="images/cards/' + card + '.png" alt=Playing card ' + card + '>';
+  return image_location;
+}
 
-  player.hand.push(dealHand());
-  dealer.hand.push(dealHand());
-  player.hand.push(dealHand());
-  console.log(player.hand);
-  console.log(dealer.hand);
-
-  function displayCard(card) {
-    var image_location = '<img src="images/cards/' + card + '.png" alt=Playing card ' + card + '>';
-    return image_location;
-  }
-
-  $('#player_hand').append(displayCard(player.hand[0]));
-  $('#dealer_hand').append(displayCard(dealer.hand[0]));
-  $('#player_hand').append(displayCard(player.hand[1]));
-
-  // Evaluate hand values
-  function handValue(hand){
-    var value = 0;
-    var aces = 0;
-    for (var i = 0; i < hand.length; i++){
-      switch (hand[i][0]) {
-        case '2':
-          value += 2; break;
-        case '3':
-          value += 3; break;
-        case '4':
-          value += 4; break;
-        case '5':
-          value += 5; break;
-        case '6':
-          value += 6; break;
-        case '7':
-          value += 7; break;
-        case '8':
-          value += 8; break;
-        case '9':
-          value += 9; break;
-        default:
-          value += 10; break;
-        case 'A':
-          value += 11;
-          aces += 1;
-          break;
-      }
+// Evaluate hand values
+function handValue(hand){
+  var value = 0;
+  var aces = 0;
+  for (var i = 0; i < hand.length; i++){
+    switch (hand[i][0]) {
+      case '2':
+        value += 2; break;
+      case '3':
+        value += 3; break;
+      case '4':
+        value += 4; break;
+      case '5':
+        value += 5; break;
+      case '6':
+        value += 6; break;
+      case '7':
+        value += 7; break;
+      case '8':
+        value += 8; break;
+      case '9':
+        value += 9; break;
+      default:
+        value += 10; break;
+      case 'A':
+        value += 11;
+        aces += 1;
+        break;
     }
-    while (aces > 0){
-      if (value > 21){
-        value -= 10;
-      }
-      aces--;
-    }
-    console.log(value);
-    console.log(aces);
-    return value;
   }
+  while (aces > 0){
+    if (value > 21){
+      value -= 10;
+    }
+    aces--;
+  }
+  console.log(value);
+  console.log(aces);
+  return value;
+}
 
-  var pv = handValue(player.hand);
-  var dv = handValue(dealer.hand);
-
-  $('#player_name').append(pv);
-  $('#dealer_name').append(dv);
-
-  // Player blackjack check
+// Player blackjack check
+function player_bj_check(pv){
   if (pv === 21){
     document.write("Congrats, you win with blackjack!");
     player.bankroll += player.bet_size * 1.5;
     player.wins === true;
   }
+}
 
-  // Deal final dealer card
-  dealer.hand.push(dealHand());
-  console.log(dealer.hand);
-  dv = handValue(dealer.hand);
-
-  // Offer insurance if dealer's first card was an A
+// Offer insurance if dealer's first card was an A
+function insurance_offer(){
   if (dealer.hand[0][0] === 'A') {
     if (confirm("Do you want to buy insurance?")){
       document.write("Hint: You never want to buy insurance in blackjack. It is always unprofitable and therefore you cannot buy it in this game.");
@@ -151,45 +131,111 @@ var initialDeal = function() {
       document.write("Good decision. Buying insurance in blackjack is a losing play over the long run.");
     }
   }
+}
 
-  // Check to see if the dealer has blackjack
+// Check to see if the dealer has blackjack
+function dealer_bj_check(dv){
   if (dv === 21){
     document.write("Sorry, the dealer has blackjack.");
     player.bankroll -= player.bet_size;
     dealer.wins === true;
   }
+}
 
-  // Create action buttons - some of these are only available on first action
+// Create stand button
+function stand_button(){
   var button_stand = document.createElement("button");
   button_stand.innerHTML = "Stand";
+  $("#stand").append(button_stand);
+}
+
+// Create hit button
+function hit_button(){
   var button_hit = document.createElement("button");
   button_hit.innerHTML = "Hit";
+  $("#hit").append(button_hit);
+}
+
+// Create double button - only available first round
+function double_button(){
   var button_double = document.createElement("button");
   button_double.innerHTML = "Double";
+  $("#double").append(button_double);
+}
+
+// Create split button
+function split_button(){
   var button_split = document.createElement("button");
   button_split.innerHTML = "Split";
+  $("#split").append(button_split);
+}
+
+// Create hint button
+function hint_button(){
   var button_hint = document.createElement("button");
   button_hint.innerHTML = "Hint";
-  var button_leave = document.createElement("button");
-  button_leave.innerHTML = "Leave";
-
-  $("#stand").append(button_stand);
-  $("#hit").append(button_hit);
-  $("#double").append(button_double);
-  $("#split").append(button_split);
   $("#hint").append(button_hint);
-  $("#leave").append(button_leave);
+}
+
+// // Create stats button
+// function stats_button(){
+//   var button_leave = document.createElement("button");
+//   button_leave.innerHTML = "Leave";
+//   $("#stats").append(button_leave);
+// }
+
+// Create stats section
+function stats() {
+  $('#bet').children().append(placeBet());
+  $('#score').children().append(score);
+  $('#hand_num').children().append(hand_num);
+  $('#bankroll').children().append(player.bankroll);
+}
+
+// Setup game
+var initial_deal = function() {
+  create_shuffled_deck();
+
+  $('#dealer_name').text(dealer.name + "'s Hand Value: ");
+  $('#player_name').text(player.name + "'s Hand Value: ");
+
+  player.hand.push(dealHand());
+  dealer.hand.push(dealHand());
+  player.hand.push(dealHand());
+  console.log(player.hand);
+  console.log(dealer.hand);
+
+  $('#player_hand').append(displayCard(player.hand[0]));
+  $('#dealer_hand').append(displayCard(dealer.hand[0]));
+  $('#player_hand').append(displayCard(player.hand[1]));
+
+  var pv = handValue(player.hand);
+  var dv = handValue(dealer.hand);
+
+  $('#player_name').append(pv);
+  $('#dealer_name').append(dv);
+
+  player_bj_check(pv);
+
+  // Deal final dealer card
+  dealer.hand.push(dealHand());
+  console.log(dealer.hand);
+  dv = handValue(dealer.hand);
+
+  insurance_offer()
+  dealer_bj_check(dv)
+
+  stand_button();
+  hit_button();
+  double_button();
+  split_button();
+  hint_button();
+
+  stats();
 };
 
 // Start game
-initialDeal();
-
-// Create stats section
-$('#bet').children().append(placeBet());
-$('#score').children().append(score);
-$('#hand_num').children().append(hand_num);
-$('#bankroll').children().append(player.bankroll);
-
+initial_deal();
 
 console.log("Remaining deck: ");
 console.log(cards);
